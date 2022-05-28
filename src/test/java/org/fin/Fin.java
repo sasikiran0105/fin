21327d3a806a0f824c7a7babfa06b79c1fba877c
sasikiran0105 <sasikiransusanth@gmail.com>
2022-05-28 19:31:41 +0530

    initial codes

---
+++ src/test/java/org/fin/Fin.java
@@ -0,0 +1,27 @@
+package org.fin;
+
+public class Fin {
+
+	public void Method1() {
+		System.out.println("1");
+	}
+public void Method2() {
+	System.out.println("2");
+	}
+public void Method3() {
+	System.out.println("3");
+}
+public void Method4() {
+	System.out.println("4");
+}
+public void Method5() {
+	System.out.println("5");
+}
+public void Method6() {
+	System.out.println("6");
+}
+public void Method7() {
+	System.out.println("7");
+}
+	
+}
